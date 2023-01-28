a83ced9a235c0b5161c85c186b23b67dd2046da9
Saower Chan <github@saower.com>
2023-01-28 10:45:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# photo.saower.com
+
---
+++ data/meta.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+const siteInfo = {
+  title: "Saower Chan \u2014 Photos",
+  description: "Photography by Saower Chan a designer living in GD, CN.",
+  fullDescription: (
+    <React.Fragment>
+        <p>风似手在轻轻滑过满沟与壑的额</p>
+        <p>长则是情怀融融展不扯眉间的锁</p>
+        <p>没人来过</p>
+        <p>我飘在了风里 飘进了风雨</p>
+        <p>叹这样心绪悠悠的黄昏与白昼</p>
+        <p>一程程水寒山冷瑟瑟风中的人</p>
+        <p>一步步隐隐天涯树远雾黄昏</p>
+        <p>谁会在意</p>
+        <p>我浪迹在这里 我流放在这里</p>
+        <p>最恨这逈野悲凉 草黄风无语</p>
+        <p>那一片悠悠的胡笛在重重叠叠</p>
+        <p>似残风铺满戈壁</p>
+        <p>我见她意汪汪情漓漓颤巍巍</p>
+        <p>扶不住岁月的叹息</p>
+        <p>谁在叹息里自斟自饮</p>
+        <p>将万里愁绪付与天地</p>
+        <a href="http://beian.miit.gov.cn">粤ICP备17145788号</a>
+
+
+      <p>
+        You can follow me on <a href="https://github.com/saower">Github</a> and{" "}
+        <a href="https://blog.saower.com">My Blog</a>.
+      </p>
+    </React.Fragment>
+  ),
+  image: "https://photo.saower.com/images/00009.jpg",
+}
+
+export default siteInfo
